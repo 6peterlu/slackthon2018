b74936eb75158fde217d698e5b6b5dd24490b047
William Wang <williamwang@Williams-MacBook-Pro.local>
2018-07-21 11:17:57 -0700

    get request body

--- easy-peasy-bot/index.js
+++ easy-peasy-bot/index.js
@@ -114,39 +114,47 @@ var url = require('url');
 var request = require('request');
 
 var bodyParser = require('body-parser');
-app.use(bodyParser.json());
+
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.set('port', (process.env.PORT || 80));
 
 app.post('/', function(req, res){
   console.log("hello");
+  console.log(req.body);
+  console.log(req.body.payload);
+
+  console.log(typeof(req.body.payload));
+
+  let obj = JSON.parse(req.body.payload);
+  console.log(obj.callback_id);
   res.send('It works!');
 });
 
-app.post('/post', function(req, res){
-  var parsed_url = url.format({
-    pathname: 'https://api.genius.com/search',
-    query: {
-      access_token: process.env.GENIUS_ACCESS,
-      q: req.body.text
-    }
-  });
-
-  request(parsed_url, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var data = JSON.parse(body);
-      var first_url = data.response.hits[0].result.url;
-
-      var body = {
-        response_type: "in_channel",
-        text: first_url
-      };
-
-      res.send(body);
-    }
-  });
-});
+// app.post('/post', function(req, res){
+//   var parsed_url = url.format({
+//     pathname: 'https://api.genius.com/search',
+//     query: {
+//       access_token: process.env.GENIUS_ACCESS,
+//       q: req.body.text
+//     }
+//   });
+//
+//   request(parsed_url, function (error, response, body) {
+//     if (!error && response.statusCode == 200) {
+//       var data = JSON.parse(body);
+//       var first_url = data.response.hits[0].result.url;
+//
+//       var body = {
+//         response_type: "in_channel",
+//         text: first_url
+//       };
+//
+//       res.send(body);
+//     }
+//   });
+// });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
